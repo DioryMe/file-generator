c5db50328c6f3cf2bdac3233a505025d8f77fb7a
Olli-Pekka Pohjola <op@diory.me>
2024-05-05 23:41:20 +0300

    chore: v0.1.2-rc11

--- src/video/date.ts
+++
@@ -1,4 +0,0 @@
-export function getDate(outputString: string): string | undefined {
-  const matchArray = outputString.match(/(?<=creation_time[\s]*:\s).*/)
-  return matchArray ? matchArray[0] : undefined
-}
